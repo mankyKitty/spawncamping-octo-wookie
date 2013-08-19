6eccf32c278c2662128d64e8504245d241ed18bc
Sean Chalmers <sclhiannan@gmail.com>
2013-08-19 16:29:01 +0200

    Being overly aggressive

--- Clojure/modern_cljs/project.clj
+++ Clojure/modern_cljs/project.clj
@@ -19,12 +19,33 @@
   
   ;; Cljsbuild options and configuration
   :cljsbuild {:builds
-              [{;;CLJS Source Code Path
-                :source-paths ["src/cljs"]
+              {
+               :dev
+               {;;CLJS Source Code Path
+                :source-paths ["src/cljs" "src/brepl"]
                 ;; Google Closure (CLS) options configuration
                 :compiler {;; CLS generated JS script filename
-                           :output-to "resources/public/js/modern.js"
+                           :output-to "resources/public/js/modern_dbg.js"
                            ;; Minimal JS optimisations
                            :optimizations :whitespace
                            ;; generated JS code prettyfication
-                           :pretty-print true}}]})
+                           :pretty-print true}}
+               :pre-prod
+               {;;same path as above
+                :source-paths ["src/cljs" "src/brepl"]
+                :compiler {;; different output name
+                           :output-to "resources/public/js/modern_pre.js"
+                           ;; Simple optimisations
+                           :optimizations :simple
+                           ;; no need for prettyfication
+                           :pretty-print false}}
+               :prod
+               {;; clojurescript source path
+                :source-paths ["src/cljs"]
+                ;; Google Closure Compiler Options
+                :compiler {;; the name of the emitted JS script
+                           :output-to "resources/public/js/modern.js"
+                           ;; Advanced optimisations
+                           :optimizations :advanced
+                           ;; No need for prettification
+                           :pretty-print false}}}})
